64050d4340d9af08c885a4a52d9985d96f1f5447
saidulislam28 <saidulislam79028@gmail.com>
2024-07-13 12:01:24 +0600

    fix some styles

--- src/component/projects/Projects.jsx
+++ src/component/projects/Projects.jsx
@@ -10,10 +10,10 @@ const Projects = () => {
     <div id='projects1'>
 
       
-<h5 id='projects' className=" text-2xl lg:text-5xl font-semibold text-white text-center my-10 border-b-4 w-[60%] lg:w-[30%] mx-auto py-5 border-yellow-400 ">My Projects</h5>
+<h5 id='projects' className=" text-2xl lg:text-5xl font-semibold text-white   text-center my-10 border-b-4 w-[60%] lg:w-[30%] mx-auto py-5 border-yellow-400 ">My Projects</h5>
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center p-3 my-10">
-      <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md ">
+      <div className="max-w-2xl overflow-hidden bg-gray-300 rounded-lg shadow-md ">
         <img
           className="object-cover w-full h-64"
           src={assignment10}
@@ -30,12 +30,12 @@ const Projects = () => {
             >
              TourOp
             </p>
-            <p className="mt-2 text-sm text-gray-600 ">
+            <p className="mt-2 text-sm text-black">
             TourOp is a dynamic web application designed for travel enthusiasts looking to explore the best tourist destinations across South Asia. With TourOp, users can effortlessly browse through a comprehensive list of stunning locations, add new places, and keep the information up-to-date by editing details about the creators of each tour place card.
             </p>
           </div>
         </div>
-        <div className='flex justify-between p-5 text-gray-500'>
+        <div className='flex justify-between p-5 text-gray-700'>
 
           <a href='https://tourop-client.netlify.app/' target='_blank' className='flex items-center gap-2 font-semibold hover:underline hover:text-black hover:font-bold'>Live Demo <LuArrowUpRightSquare /></a>
           <a href='https://github.com/saidulislam28/TourOP-client' target='_blank' className='flex items-center gap-2 font-semibold hover:underline hover:text-black hover:font-bold'>Github <IoLogoGithub />
@@ -44,7 +44,7 @@ const Projects = () => {
 
         </div>
       </div>
-      <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md ">
+      <div className="max-w-2xl overflow-hidden bg-gray-300  rounded-lg shadow-md ">
         <img
           className="object-cover w-full h-64"
           src={assignment11}
@@ -61,12 +61,12 @@ const Projects = () => {
             >
              StudyMate
             </p>
-            <p className="mt-2 text-sm text-gray-600 ">
+            <p className="mt-2 text-sm text-black">
             Study Mate is an innovative web platform designed to enhance collaborative learning among students. It serves as a hub where students can manage their academic tasks efficiently by taking on assignments, creating tasks for peers, and providing constructive feedback.
             </p>
           </div>
         </div>
-        <div className='flex justify-between p-5 text-gray-500'>
+        <div className='flex justify-between p-5 text-gray-700'>
 
           <a href='https://elegant-torrone-7cb8a9.netlify.app/' target='_blank' className='flex items-center gap-2 font-semibold hover:underline hover:text-black hover:font-bold'>Live Demo <LuArrowUpRightSquare /></a>
           <a href='https://github.com/saidulislam28/studymate-client'
@@ -76,7 +76,7 @@ const Projects = () => {
 
         </div>
       </div>
-      <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md ">
+      <div className="max-w-2xl overflow-hidden bg-gray-300 rounded-lg shadow-md ">
         <img
           className="object-cover w-full h-64"
           src={assignment12}
@@ -93,12 +93,12 @@ const Projects = () => {
             >
              MicroEarn
             </p>
-            <p className="mt-2 text-sm text-gray-600 ">
+            <p className="mt-2 text-sm text-black">
             Micro Earn is an innovative platform designed to facilitate micro-earning opportunities for users. It provides a streamlined environment where individuals can perform small tasks, complete various assignments, and earn rewards.
             </p>
           </div>
         </div>
-        <div className='flex justify-between p-5 text-gray-500'>
+        <div className='flex justify-between p-5 text-gray-700'>
 
           <a href='https://micro-earn-authentication.web.app/' target='_blank' className='flex items-center gap-2 font-semibold hover:underline hover:text-black hover:font-bold'>Live Demo <LuArrowUpRightSquare /></a>
           <a href='https://github.com/saidulislam28/micro-earn-client'  target='_blank'className='flex items-center gap-2 font-semibold hover:underline hover:text-black hover:font-bold'>Github <IoLogoGithub />
